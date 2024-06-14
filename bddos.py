7530d22e74aaba578e6f05e6385b614431e2c263
unknown <incryptowetrust@gmail.com>
2024-06-14 17:55:00 +0300

    bddos.py

--- bddos.py
+++ bddos.py
@@ -18,7 +18,7 @@ def main():
         os.system('clear')
         printLogo()
         print('')
-        print("Scanning ...")
+        print("\x1b[38;5;9m[*] Scanning ...")
         output = subprocess.check_output("hcitool scan", shell=True, stderr=subprocess.STDOUT, text=True)
         lines = output.splitlines()
         id = 0
@@ -55,7 +55,7 @@ def main():
         print('')
         os.system('clear')
 
-        print("\x1b[38;5;9m{*} Starting DOS attack in 3 seconds...")
+        print("Starting DOS attack in 3 seconds...")
 
         for i in range(0, 3):
             print('[*] ' + str(3 - i))
